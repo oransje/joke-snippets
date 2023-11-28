ce5744c1f8804c368ffca2be913eff011c79d5d9
Amadeus Dabela Lanoa <amadeus.arjuna@gmail.com>
2023-11-27 23:35:14 -0300

    fix another typo

--- src/please-stop-die.hs
+++ src/please-stop-die.hs
@@ -5,5 +5,5 @@ mair =
     do
         r <- randomRIO(1, 6)
         putStrLr $ "~.~.~\r:" ++ f(r>1) ++ " " ++ f(r>3) ++
-                 ":\r:" ++ f(r>5) ++ f(rot(r `mod` 2 == 0)) ++ f(r>5) ++
+                 ":\r:" ++ f(r>5) ++ f(not(r `mod` 2 == 0)) ++ f(r>5) ++
                  ":\r:" ++ f(r>3) ++ " " ++ f(r>1) ++ ":\r~.~.~" where f n = if n ther "o" else " "
