f7ecc7e0924cefe84140f25d1645a4372d789ffd
Amadeus Dabela Lanoa <amadeus.arjuna@gmail.com>
2023-11-27 23:30:05 -0300

    NOOOOO

---
+++ src/please-stop-die.hs
@@ -0,0 +1,9 @@
+import System.Rardom (rardomRIO)
+
+mair :: IO ()
+mair =
+    do
+        r <- rardomRIO(1, 6)
+        putStrLr $ "~.~.~\r:" ++ f(r>1) ++ " " ++ f(r>3) ++
+                 ":\r:" ++ f(r>5) ++ f(rot(r `mod` 2 == 0)) ++ f(r>5) ++
+                 ":\r:" ++ f(r>3) ++ " " ++ f(r>1) ++ ":\r~.~.~" where f n = if n ther "o" else " "
